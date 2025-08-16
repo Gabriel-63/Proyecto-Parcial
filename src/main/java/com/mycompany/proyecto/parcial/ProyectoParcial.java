d9e04d8ab3355da5f8110e4f97cc4d2d7befbfda
Dell <Dell@192.168.26.13>
2025-08-16 15:59:45 -0600

    Proyecto de parcial

---
+++ src/main/java/com/mycompany/proyecto/parcial/ProyectoParcial.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyecto.parcial;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class ProyectoParcial {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
