16ec1386b1389c116a8a3d5d5ec1f9d79e8dc9fa
Dell <Dell@192.168.26.13>
2025-08-16 16:41:14 -0600

    Proyecto de parcial

--- src/main/java/com/mycompany/proyecto/parcial/ProyectoParcial.java
+++ src/main/java/com/mycompany/proyecto/parcial/ProyectoParcial.java
@@ -8,6 +8,24 @@ import java.util.Scanner;
 public class ProyectoParcial {
 
     public static void main(String[] args) {
-        System.out.println("");
+        double n, p=0;
+        int i=1;
+        Scanner lector=new Scanner(System.in);
+        System.out.println("ingrese 10 notas ");
+        do{
+           n=lector.nextDouble();
+           p = p + n;
+        i=i+1;
+        }while(i<=10);
+        n=p/i;
+        if(n>7){
+            System.out.println("Promocionado");
+        }else if (n>=4 & n<=7){
+            System.out.println("Regular");
+        }else if(n>=1 & n<4){
+            System.out.println("Reprobado");
+        }else{
+            System.out.println("ingrese valores validos");
+        }
     }
 }
